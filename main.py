b91adc8231a49acb2a74817acc7d856ba67cbb1e
RealTribn <tristan.vandermeer@gmail.com>
2025-05-26 15:42:09 +0100

    Starting app and quit listener

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # HIYA :3
+
+from textual.app import App
+from textual.events import Key
+
+class MainHub(App):
+    def on_key(self, event: Key) -> None:
+        if event.key == "q":
+            self.exit()
+
+if __name__ == "__main__":
+    
+    Main = MainHub()
+    Main.run()
+
