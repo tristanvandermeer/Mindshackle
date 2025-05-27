f15095e51eb77d3bfff28f633b8dd495cb212495
RealTribn <tristan.vandermeer@gmail.com>
2025-05-27 11:05:29 +0100

    Colour Event

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from textual.app import App
 from textual.events import Key
 
 class MainHub(App):
+
     def on_key(self, event: Key) -> None:
         if event.key == "q":
             self.exit()
+
+    def on_mount(self):
+        self.screen.styles.background = "hotpink"
 
 if __name__ == "__main__":
     
